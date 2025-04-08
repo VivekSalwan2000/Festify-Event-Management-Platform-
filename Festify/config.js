03dd00f3fa47514850215d420f95dffe4ca63230
Gautam <gg21qv@brocku.ca>
2025-04-08 18:45:40 -0400

    Testing config.js

---
+++ Festify/config.js
@@ -0,0 +1,7 @@
+const config = {
+    FIREBASE_API_KEY: "AIzaSyDe0ZCrJCtspANzB-is2Hh8gvkyvLNcRmA",
+    EMAIL_SERVICE_ID: "service_0k8kvpq",
+    OPENAI_API_KEY: "OUR_OPENAI_API_KEY"
+  };
+  
+  export { config };
